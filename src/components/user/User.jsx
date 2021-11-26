e628aaaaa44b51914e5c066930d604011a2939ec
aleks <aleks.mihaylov69@gmail.com>
2021-11-26 16:55:12 +0200

    feat: created User component

---
+++ src/components/user/User.jsx
@@ -0,0 +1,33 @@
+import { Container, Grid, Avatar, Typography } from "@mui/material";
+import styles from "./User.module.scss";
+import classNames from "classnames";
+
+export default function User({ username, avatar, size = 69 }) {
+  return (
+    <div className={classNames(styles["user-container"])}>
+      <Container className={classNames(styles.container)} maxWidth="xl">
+        <Grid
+          container
+          spacing={2}
+          direction="row"
+          justifyContent="space-around"
+          alignItems="center"
+        >
+          <Grid item xs={4}>
+            <Avatar
+              className={classNames(styles.avatar)}
+              alt="user-avatar"
+              src={avatar}
+              sx={{ width: size, height: size }}
+            />
+          </Grid>
+          <Grid item xs={8}>
+            <Typography variant="body1" className={classNames(styles.username)}>
+              {username}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+}
