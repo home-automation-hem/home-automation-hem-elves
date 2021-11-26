ac3c525d3fefae68738497964f9022bb42aab5c5
Kristiyan Ivanov <kristianiva88@gmail.com>
2021-11-27 01:10:11 +0200

    feat: implement Time component

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -3,11 +3,14 @@ import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Link from "../../src/components/link/Link";
+import Time from "../../src/components/time/Time";
 
 export default function About() {
   return (
     <Container maxWidth="sm">
-      <Grid container justifyContent="center">
+        <Time />
+
+        <Grid container justifyContent="center">
         <Grid item>
           <Button variant="contained" component={Link} noLinkStyle href="/">
             🏠 Home
