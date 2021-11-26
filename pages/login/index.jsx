598333fe0b2be8ef19595981ccaeacc380ae427a
Kristiyan Ivanov <kristianiva88@gmail.com>
2021-11-27 01:35:00 +0200

    fix: ignore .idea/

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -5,38 +5,39 @@ import Link from "../../src/components/link/Link";
 import styles from '../../src/styles/LoginPage.module.scss';
 
 export default function Login() {
-    return (
-        <div className={'wrapper'}>
-            <Container  maxWidth="sm"
-                sx={{
-                    '& > :not(style)': {
-                        mt: "50%",
-                    },
-                }}
-            >
-                <Box
-                    sx={{
-                        alignItems: "center",
-                        justifyContent: "center",
-                        display: 'flex',
-                        flexWrap: 'wrap',
-                        flexDirection: "column",
-                        '& > :not(style)': {
-                            m: 1,
-                            minWidth: 128,
-                            minHeight: 128,
-                        },
-                    }}
-                >
-                    <Paper elevation={1}>
-                        <LoginComponent />
-                    </Paper>
+  return (
+    <div className={'wrapper'}>
+      <Container
+        maxWidth="sm"
+        sx={{
+          '& > :not(style)': {
+            mt: "50%",
+          },
+        }}
+      >
+        <Box
+          sx={{
+            alignItems: "center",
+            justifyContent: "center",
+            display: 'flex',
+            flexWrap: 'wrap',
+            flexDirection: "column",
+            '& > :not(style)': {
+              m: 1,
+              minWidth: 128,
+              minHeight: 128,
+            },
+          }}
+        >
+          <Paper elevation={1}>
+            <LoginComponent />
+          </Paper>
 
-                    <small>
-                        Don't have an account? <a href={''}>Register today!</a>
-                    </small>
-                </Box>
-            </Container>
-        </div>
-    );
+          <small>
+            Don't have an account? <a href={''}>Register today!</a>
+          </small>
+        </Box>
+      </Container>
+    </div>
+  );
 };
