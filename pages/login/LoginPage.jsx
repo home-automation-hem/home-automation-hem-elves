e32bd69443b13a09d3f9aa4287ca0b03bcd0d073
Kristiyan Ivanov <kristianiva88@gmail.com>
2021-11-27 15:50:19 +0200

    fix: LoginPage naming

---
+++ pages/login/LoginPage.jsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { Grid, Button, Container, Paper, Box } from "@mui/material";
+import LoginComponent from '../../src/components/login/Login.jsx';
+import Link from "../../src/components/link/Link";
+import styles from '../../src/styles/LoginPage.module.scss';
+
+export default function LoginPage() {
+  return (
+    <div className={styles.wrapper}>
+      <Container
+        maxWidth="sm"
+        sx={{
+          '& > :not(style)': {
+            mt: "50%",
+          },
+        }}
+      >
+        <Box
+          sx={{
+            alignItems: "center",
+            justifyContent: "center",
+            display: 'flex',
+            flexWrap: 'wrap',
+            flexDirection: "column",
+            '& > :not(style)': {
+              m: 1,
+              minWidth: 128,
+              minHeight: 128,
+            },
+          }}
+        >
+          <Paper elevation={1}>
+            <LoginComponent />
+          </Paper>
+
+          <small>
+            Don't have an account? <a href={''}>Register today!</a>
+          </small>
+        </Box>
+      </Container>
+    </div>
+  );
+};
