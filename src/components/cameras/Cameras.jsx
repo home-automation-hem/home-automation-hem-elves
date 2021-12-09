adeb33ab014a5d8e92ac23a551dbad11b7af8215
Kristiyan Ivanov <kristianiva88@gmail.com>
2021-12-09 13:16:38 +0200

    feat: add cameras

---
+++ src/components/cameras/Cameras.jsx
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import styles from './Cameras.module.scss';
+import Card from '../card/Card.jsx';
+import { Grid } from '@mui/material';
+
+export default function Cameras ({ cameras, hasButton }) {
+  const [current, setCurrent] = useState('');
+
+  return (
+    <div>
+     <Card
+       autoplay={true}
+       width={500}
+       height={400}
+       iconUrl={current?.videoUrl || ''}
+       outlined={hasButton}
+     />
+
+      <Grid container width={802} height={403}>
+        {cameras.map((camera, idx) => (
+          <Grid
+            item
+            xs={4}
+            md={4}
+            width={267}
+            height={201}
+            sx={{ backgroundColor: '#EBE6F7' }}
+          >
+            <div onClick={() => setCurrent(camera)}>
+              <Card
+                key={idx}
+                iconUrl={camera.videoUrl}
+                outlined={hasButton}
+              />
+            </div>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
+};
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import styles from './Card.module.scss';
 import Card from '@mui/material/Card';
 import Grid from '@mui/material/Grid';
 
-export default function Index({ iconUrl, onClick, outlined = false }) {
+export default function Index({ autoplay, iconUrl, onClick, outlined = false }) {
+  const isVideo = iconUrl.includes('videos');
+
   return (
     <Card className={styles.card} variant={outlined ? 'outlined' : ''}>
-      {iconUrl ? <img width={'40%'} src={iconUrl} alt="" /> : ''}
+      {iconUrl ? (
+        isVideo ? (
+          <video width={'100%'} src={iconUrl} autoplay={autoplay ? 'autoplay' : null} alt="iconUrl" />
+        ) : <img width={'40%'} src={iconUrl} alt="iconUrl" />
+      ) : ''}
     </Card>
   );
 }
