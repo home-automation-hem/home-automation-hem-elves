ee21d76b74680b07fd0803ab0206e972e8dd93a1
Kristiyan Ivanov <kristianiva88@gmail.com>
2021-12-10 22:49:38 +0200

    feat: create dashboard page

---
+++ pages/dashboard/index.jsx
@@ -0,0 +1,7 @@
+import { Typography } from '@mui/material';
+import Dashboard  from '../../src/components/dashboard/Dashboard.jsx';
+
+export default function Index() {
+  // return <Example />;
+  return <Dashboard />;
+}
