3bb46da39c2bf8acd920e583dfa77f85eeacce93
Kristiyan Ivanov <kristianiva88@gmail.com>
2021-12-21 13:58:23 +0200

    fix: remove variant prop

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,7 +4,7 @@ import Card from '@mui/material/Card';
 import { Container, Typography } from '@mui/material';
 import Chip from '@mui/material/Chip';
 
-export default function Index({ autoPlay, title, /*variant,*/ iconUrl, outlined = false }) {
+export default function Index({ autoPlay, title, variant, iconUrl, outlined = false }) {
   const isVideo = iconUrl?.includes('videos') ||  iconUrl?.includes('mp4');
   const style = null;
 
@@ -12,29 +12,40 @@ export default function Index({ autoPlay, title, /*variant,*/ iconUrl, outlined 
   // else if (variant === "offline") {}
   // else if (variant === "on") {}
 
-  return (
-    <Card className={styles.card} variant={outlined ? 'outlined' : ''}>
-      {/*<Chip label={""} />*/}
+  // if (variant === "off") {}
+  // else if (variant === "offline") {}
+  // else if (variant === "on") {}
+  let shouldRender =
+    variant?.toLowerCase() === "off" ||
+    variant?.toLowerCase() === "offline" ||
+    variant?.toLowerCase() === "on";
 
-      <Typography variant="h3">{title}</Typography>
-      {
-        title?.toLowerCase() === "offline".toLowerCase() ? <Chip label={"!"} /> : null
-        // variant?.toLowerCase() === "offline".toLowerCase() ? <Chip label={"!"} /> : null
-      }
-      {iconUrl ? (
-        isVideo ? (
-          <video
-            width={'100%'}
-            src={iconUrl}
-            autoPlay={autoPlay ? 'autoplay' : null}
-            autoplay={autoPlay ? 'autoplay' : null}
-            alt="iconUrl" />
-        ) : <React.Fragment>
-          <img width={'40%'} src={iconUrl} alt="iconUrl" />
-        </React.Fragment>
-      ) : <React.Fragment>
-        <img width={'40%'} src={""} alt="iconUrl" />
-      </React.Fragment> }
-    </Card>
+  return (
+    shouldRender ? (
+        <Card className={styles.card} variant={outlined ? 'outlined' : ''}>
+          {/*<Chip label={""} />*/}
+
+          <Typography variant="h3">{title}</Typography>
+          {
+            title?.toLowerCase() === "offline".toLowerCase() ? <Chip label={"!"} /> : null
+            // variant?.toLowerCase() === "offline".toLowerCase() ? <Chip label={"!"} /> : null
+          }
+          {iconUrl ? (
+            isVideo ? (
+              <video
+                width={'100%'}
+                src={iconUrl}
+                autoPlay={autoPlay ? 'autoplay' : null}
+                autoplay={autoPlay ? 'autoplay' : null}
+                alt="iconUrl" />
+            ) : <React.Fragment>
+              <img width={'40%'} src={iconUrl} alt="iconUrl" />
+            </React.Fragment>
+          ) : <React.Fragment>
+            <img width={'40%'} src={iconUrl} alt="iconUrl" />
+          </React.Fragment> }
+        </Card>
+      )
+     : null
   );
 }
