d818ab44b3adb8bb7b94d656d6763cb1312c2c81
Kristiyan Ivanov <kristianiva88@gmail.com>
2021-12-21 11:29:32 +0200

    fix: component

--- src/components/devices/Devices.jsx
+++ src/components/devices/Devices.jsx
@@ -6,22 +6,24 @@ export default function Devices({ devices }) {
   return (
       
     <div className={`${styles['devices-wrapper']}`}>
-      <Grid container  width={802} height={403} display={'flex'} justifyContent={'center'}>
-     
+      <Grid container width={802} height={403} display={'flex'} justifyContent={'center'}>
         {devices.map((device, idx) => (
           <Grid
-          p={2}
+            p={2}
             item
             xs={4}
             md={4}
             maxWidth={267}
             minHeight={201}
-          ><Card key={idx} iconUrl={device.iconUrl} title={device.title} />
-          
+          >
+            <Card
+              key={idx}
+              iconUrl={device.iconUrl}
+              title={device.title}
+              variant={device.title} />
           </Grid>
         ))}
        </Grid >
-    
     </div>
   );
 }
