7159edb9d56ff89f203d1663c11609d1f249e1eb
Stephanie <stephanieilieva1995@gmail.com>
2021-12-22 14:28:17 +0200

    sceneComposer last changes...

--- src/components/scenes/SceneComposer.jsx
+++ src/components/scenes/SceneComposer.jsx
@@ -1,28 +1,40 @@
 import styles from './SceneComposer.module.scss';
 import { Container, Grid, Typography } from '@mui/material';
 import Devices from '../devices/Devices.jsx';
-import Rooms from '../rooms/Rooms.jsx';
+import Card from '../card/Card.jsx';
+import Scenes from './Scenes.jsx'
+import _ from 'lodash';
+
 
 export default function SceneComposer({ devices, rooms, selected, onScene }) {
   //state of selection should be in the parent component
   //value is the selected scene
   //useMemo to optimize the grouping
-  //rooms how to vsualize for each room devixe
+  //rooms how to visualize for each room device
+
+
+const chunked = _.chunk(rooms, 1);
+
+// console.log(chunked);
+  
   return (
     <div className={`${styles['scene-composer']}`}>
       <Container>
         <Grid container>
           <Grid item xs={12}>
-            <Typography></Typography>
+            <Typography>
+                  {chunked.map(cards => {
+                    return (<>
+                      <Scenes
+                      cards={cards}
+                      /> <Devices devices={devices} value={selected} onScene={onScene} /></>
+                    );
+                  })}  
+            </Typography>
+           
           </Grid>
           <Grid item xs={12}>
-            <Devices devices={devices} value={selected} onScene={onScene} />
-          </Grid>
-          <Grid item xs={12}>
-            <Typography></Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Devices devices={devices} value={selected} onScene={onScene} />
+            
           </Grid>
         </Grid>
       </Container>
